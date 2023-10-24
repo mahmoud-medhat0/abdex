3dccaad6865f282f9216e19d5cf62cede003247f
mahmoud-medhat0 <hhetlar120@gmail.com>
2023-10-24 23:57:47 +0300

    fix governate model

--- app/Models/Center.php
+++ app/Models/Center.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Center extends Model
 {
     use HasFactory;
+    protected $table='governorates';
     protected $with=['governate'];
     public function governate()
     {
